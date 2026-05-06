5d4eaf8c1c4eaa4ab09644408f7020bbf018592e
Muhammad Halim <74248915+HLime-dev@users.noreply.github.com>
2026-05-06 19:43:11 +0800

    Update fias.lua

--- fias.lua
+++ fias.lua
@@ -1,8 +1,14 @@
+local filename = "game_dump.txt"
+
+if isfile and isfile(filename) then
+    delfile(filename)
+end
+
 local data = ""
 
 for _, v in pairs(game:GetDescendants()) do
     data = data .. v:GetFullName() .. "\n"
 end
 
-writefile("game_dump.txt", data)
-print("Selesai! Disimpan ke game_dump.txt")
+writefile(filename, data)
+print("Saved to:", filename)
