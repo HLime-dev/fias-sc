33223e89ba99cc8c690583b5f757568815ceae3b
Muhammad Halim <74248915+HLime-dev@users.noreply.github.com>
2026-05-06 19:34:18 +0800

    Update fias.lua

--- fias.lua
+++ fias.lua
@@ -1,3 +1,13 @@
+local ScreenGui = Instance.new("ScreenGui", game.CoreGui)
+
+local TextBox = Instance.new("TextBox", ScreenGui)
+TextBox.Size = UDim2.new(0,400,0,300)
+TextBox.Position = UDim2.new(0,10,0,10)
+TextBox.TextWrapped = true
+TextBox.TextXAlignment = Enum.TextXAlignment.Left
+TextBox.TextYAlignment = Enum.TextYAlignment.Top
+TextBox.Text = ""
+
 for _, v in pairs(game:GetDescendants()) do
-    print(v:GetFullName())
+    TextBox.Text = TextBox.Text .. v:GetFullName() .. "\n"
 end
