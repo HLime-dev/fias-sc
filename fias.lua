d48908e3a0b1c5546785d8adb33a462bd5838a83
Muhammad Halim <74248915+HLime-dev@users.noreply.github.com>
2026-05-06 19:38:05 +0800

    Update fias.lua

--- fias.lua
+++ fias.lua
@@ -1,13 +1,8 @@
-local ScreenGui = Instance.new("ScreenGui", game.CoreGui)
-
-local TextBox = Instance.new("TextBox", ScreenGui)
-TextBox.Size = UDim2.new(0,400,0,300)
-TextBox.Position = UDim2.new(0,10,0,10)
-TextBox.TextWrapped = true
-TextBox.TextXAlignment = Enum.TextXAlignment.Left
-TextBox.TextYAlignment = Enum.TextYAlignment.Top
-TextBox.Text = ""
+local data = ""
 
 for _, v in pairs(game:GetDescendants()) do
-    TextBox.Text = TextBox.Text .. v:GetFullName() .. "\n"
+    data = data .. v:GetFullName() .. "\n"
 end
+
+writefile("game_dump.txt", data)
+print("Selesai! Disimpan ke game_dump.txt")
